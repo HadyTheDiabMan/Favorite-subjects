008d56122e8f8d008bda034182c70d81a5803ceb
hdiab22 <42975537+HadyTheDiabMan@users.noreply.github.com>
2018-10-24 12:30:31 -0400

    Add files via upload

---
+++ src/FavoriteSubject.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Hady Diab, Favorite Subjects
+ *
+ */
+public class FavoriteSubject 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("My favorite subjects:");
+		System.out.print("\tIntro to Java");
+		System.out.print("\tGeometry");
+		System.out.print("\tHealth");
+
+	
+	}
+
+
+}
